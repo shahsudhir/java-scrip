d85716e8a4b6d45c33b78d0cfcd1b3614c56f3b3
shahsudhir <111883245+shahsudhir@users.noreply.github.com>
2025-11-19 17:36:59 +0000

    Datesss

---
+++ 01_basics/dates.js
@@ -0,0 +1,10 @@
+// const date= new Date()
+// console.log(date.toJSON())
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.toString());
+// console.log(typeof date)
+
+const NewDaate= new Date()
+console.log(NewDaate);
