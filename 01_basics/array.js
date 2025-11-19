2f094e4c578142463dda1386d7ce275dce305700
shahsudhir <111883245+shahsudhir@users.noreply.github.com>
2025-11-19 17:36:38 +0000

    Array

---
+++ 01_basics/array.js
@@ -0,0 +1,34 @@
+//array
+// const number=[0,4,5,6,6,99]
+// console.log(number)
+
+//Array methid
+// number.push(8)
+// console.log(number);
+// //shift
+// number.shift()
+// console.log(number)
+
+// const array1=[1,2,3,4,5]
+// console.log(array1)
+
+// array1.unshift(6)
+// console.log(array1)
+// array1.shift()
+// console.log(array1)
+
+//includes
+
+// console.log(array1.indexOf(5))
+// console.log(array1.includes(2))
+
+// const newArray=array1.join()
+// console.log( typeof newArray)
+
+
+//slice/splice
+// const array2=array1.slice(1,4)
+// console.log(array2)
+
+// const array3=array1.splice(1,4)
+// console.log(array3);
