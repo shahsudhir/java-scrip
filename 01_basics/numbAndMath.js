fc9e73dc1cf3600c8fac4eb622baa5cdf2093be3
shahsudhir <111883245+shahsudhir@users.noreply.github.com>
2025-11-19 07:29:50 +0000

    Math and number

---
+++ 01_basics/numbAndMath.js
@@ -0,0 +1,17 @@
+// const score=400
+// console.log(score)
+// const balance=new Number(100)
+// console.log(balance.toFixed(2));
+
+// const hundred=100000
+// console.log(hundred.toLocaleString());
+
+//these all are about numbers story
+
+//******************************Maths ******************/
+// console.log(Math.random())
+// console.log(Math.random()*10+1);
+const min=10
+const max=20
+
+console.log((Math.floor (Math.random()*10)+1)+min)
