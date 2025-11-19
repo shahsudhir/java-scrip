a1f7dab764c46d9486653f09babcd20b95c77f89
shahsudhir <111883245+shahsudhir@users.noreply.github.com>
2025-11-19 07:30:10 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,5 @@
+const name="sudhir"
+const newName="danny"
+console.log(`Hello my name is ${name} and my newName is ${newName}`);
+const AnotherName="dannydolpahahaha"
+console.log(AnotherName.slice(-1,5))
