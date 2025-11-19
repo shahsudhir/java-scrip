efe0b4a77f559053b51e7b9b2360aa655b9b8d80
shahsudhir <111883245+shahsudhir@users.noreply.github.com>
2025-11-19 05:36:28 +0000

    change in datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,15 +1,55 @@
-"use Strict"//treat all js code as newer version
-//alert(3+3)// we are using node.js not the  brwoser 
+// "use Strict"//treat all js code as newer version
+// //alert(3+3)// we are using node.js not the  brwoser 
 
-console.log(3+3)
-console.log("sudhir")
-//code readability should be high
-//number=> 2 to power 53
-//bigint
-//string=> ""
-//boolean => true/false
-//null=> standalone value
-//undefined=>
-    //symbol=> unique
-console.log(typeof undefined);
-console.log(typeof null);
+// console.log(3+3)
+// console.log("sudhir")
+// //code readability should be high
+// //number=> 2 to power 53
+// //bigint
+// //string=> ""
+// //boolean => true/false
+// //null=> standalone value
+// //undefined=>
+//     //symbol=> unique
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+// //there are two types of data 1. primitive data types and another is non primitive data types 
+// //its all about how data is stored and how to accesss that data thats set 
+// //primitive data types
+// //1. String 2. 3.Number, 4.Boolean, 5.null, 6. undefined7. symbol
+
+// const id=Symbol('123');
+// const anotherId=Symbol('123')
+// console.log(id===anotherId);
+// const bigNumber=1234567778n
+// console.log( typeof bigNumber)
+
+//stack (primitive), Heap(Non-primitive)
+
+//stack
+let myYoutubename="sudhirshahcom"
+let anotherChanelName=myYoutubename
+console.log(anotherChanelName);
+
+//the real variebles are not change becose its gives to another chelname as a copy of myyoutubename not the original
+
+//heap
+let user={
+    email:"shahsudhir721@gmail.com",
+    phone:"9869028756",
+    age:26
+
+
+}
+
+const newUser=user;
+newUser.email="shahdannydolpa@gmail.com"
+newUser.age=10
+
+console.log(newUser);
+console.log(user);
+
+//this is a heap memory its not take as copy as in stack memory its take 
+// the original data and when its update its real value also change  
+
--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,21 +1,53 @@
-let score=false
-console.log(typeof score);
+// let score=false
+// console.log(typeof score);
 
-let valueInNumber=Number(score);
-console.log( typeof valueInNumber);
-console.log(  valueInNumber);
+// let valueInNumber=Number(score);
+// console.log( typeof valueInNumber);
+// console.log(  valueInNumber);
 
-//"33"=> 33
-//"33qwe"=> Not a number nan
-//true=1, false=0
+// //"33"=> 33
+// //"33qwe"=> Not a number nan
+// //true=1, false=0
 
-let isLogedIn=1
+// let isLogedIn=1
 
-let booleanLoggedIn=Boolean(isLogedIn)
-console.log( booleanLoggedIn)
+// let booleanLoggedIn=Boolean(isLogedIn)
+// console.log( booleanLoggedIn)
 
-let someNumber=44
-let convertStringNumber=String(someNumber)
-console.log(convertStringNumber)
-console.log(typeof convertStringNumber);
+// let someNumber=44
+// let convertStringNumber=String(someNumber)
+// console.log(convertStringNumber)
+// console.log(typeof convertStringNumber);
 
+//*********************************************operations********************** */
+// let value=3
+// let negValue=-value
+// console.log(negValue);
+
+let str1="hello"
+let str2="sudhirrr"
+let str3=str1-str2
+console.log(str3);
+  console.log(+"");
+
+  let num1, num2, num3
+  nu1=num2=num3=2+2
+
+  let gameCounter=100
+  gameCounter++;
+  console.log(gameCounter);
+  
+
+  let x=3;
+  const y=x++;
+  console.log(`x:${x},y:${y}`);
+//If used postfix, with operator after operand (for example, x++),
+//  the increment operator increments and returns the value before incrementing.
+
+
+  let a=3;
+  const b=++a;
+
+  console.log(`a:${a}, b:${b}`);
+//If used prefix, with operator before operand (for example, ++x), 
+// the increment operator increments and returns the value after incrementing.
